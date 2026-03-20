72b656d05491630fa8c98049fb41cf8fc9ec58f4
jeremymwood <jeremy.mason.wood@gmail.com>
2026-03-20 16:16:19 -0500

    Adjust mobile footer to two-row layout with left about/email and right social

--- client/src/components/AppFooter.tsx
+++ client/src/components/AppFooter.tsx
@@ -2,7 +2,7 @@ function AppFooter() {
   return (
     <footer className="app-footer">
       <div className="footer-divider" aria-hidden="true"></div>
-      <div className="footer-group">
+      <div className="footer-group footer-about">
         <a
           href="https://www.linkedin.com/in/jeremy-mason-wood/"
           target="_blank"
@@ -11,15 +11,15 @@ function AppFooter() {
           About me
         </a>
       </div>
-      <div className="footer-group email-container">
+      <div className="footer-group email-container footer-email">
         <a href="mailto:jeremy.mason.wood@gmail.com">
           <i className="fa-regular fa-envelope"></i>
           <span>jeremy.mason.wood@gmail.com</span>
         </a>
       </div>
-      <div className="footer-group"></div>
-      <div className="footer-group"></div>
-      <div className="footer-group">
+      <div className="footer-group footer-spacer"></div>
+      <div className="footer-group footer-spacer"></div>
+      <div className="footer-group footer-social">
         <a
           href="https://www.linkedin.com/in/jeremy-mason-wood/"
           target="_blank"
